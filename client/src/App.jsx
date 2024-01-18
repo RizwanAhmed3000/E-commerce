1f6e2fc81aa03a79e69d95b24497fd343e6426d8
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-01-18 14:10:42 +0500

    Routing and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
 import './App.css';
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Home from './Pages/Home/Home';
+import Products from './Pages/Products/Products';
+import Product from './Pages/Product/Product';
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/">
+            <Route index element={<Home />} />
+            <Route path="products">
+              <Route path=":id" element={<Products />} />
+            </Route>
+            <Route path="product">
+              <Route path=":id" element={<Product />} />
+            </Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
