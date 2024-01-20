4ccc9d696245a149a2ea7add5325269eb6a07627
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-01-20 13:13:11 +0500

    footer in progress

--- client/src/Components/Footer/Footer.jsx
+++ client/src/Components/Footer/Footer.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import './footer.scss'
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <div className='footer'>
+            <div className="top">
+                <div className="item">
+                    <h1>
+                        Categories
+                    </h1>
+                    <span>Men</span>
+                    <span>Women</span>
+                    <span>Children</span>
+                    <span>Accessories</span>
+                    <span>New Arrivals</span>
+                </div>
+                <div className="item">
+                    <h1>
+                        Links
+                    </h1>
+                    <span>FAQ</span>
+                    <span>Pages</span>
+                    <span>Stores</span>
+                    <span>Compare</span>
+                    <span>Cookies</span>
+                </div>
+                <div className="item">
+                    <h1>About</h1>
+                    <span>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Numquam, fuga dignissimos ad rerum maiores deserunt laboriosam dolorum? Laudantium earum molestias eum! Sapiente omnis non deleniti dolores commodi, ex recusandae maxime?</span>
+                </div>
+                <div className="item">
+                    <h1>Contact</h1>
+                    <span>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Vero voluptatem ut ab, explicabo nulla sequi repudiandae culpa asperiores officia saepe repellendus delectus minus quibusdam voluptate ipsa nostrum, atque at velit.</span>
+                </div>
+            </div>
+            <div className="bootom"></div>
+        </div>
     )
 }
 
