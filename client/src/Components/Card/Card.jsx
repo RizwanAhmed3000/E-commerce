39563e4866cd273fd87b97573283ae01c884bb54
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-01-23 00:24:45 +0500

    trending & featured Cards

--- client/src/Components/Card/Card.jsx
+++ client/src/Components/Card/Card.jsx
@@ -1,8 +1,22 @@
 import './card.scss'
+import { Link } from 'react-router-dom'
 
-const Card = () => {
+const Card = ({ item }) => {
     return (
-        <div className='card'>Card</div>
+        <Link className='link' to={`/product/${item.id}`}>
+            <div className='card'>
+                <div className="image">
+                    {item?.isNew && <span>New Arrivals</span>}
+                    <img src={item?.img} alt="" className="mainImg" />
+                    <img src={item?.img2} alt="" className="secImg" />
+                </div>
+                <h2>{item?.title}</h2>
+                <div className="prices">
+                    <h3>${item.oldPrice}</h3>
+                    <h3>${item.newPrice}</h3>
+                </div>
+            </div>
+        </Link>
     )
 }
 
