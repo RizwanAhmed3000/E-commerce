c31fa8ee591c683eddd2e8fd44b328adaf912cba
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-01-25 23:44:21 +0500

    products page start

--- client/src/Pages/Products/Products.jsx
+++ client/src/Pages/Products/Products.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import './products.scss'
 
 const Products = () => {
     return (
-        <div>Products</div>
+        <div className='products'>
+            <div className="left">
+                <div className="filterItem">
+                    <h2>Product Categories</h2>
+                    <div className="inputItem">
+                        <input type="checkbox" id="1" value={1}/>
+                        <label htmlFor="1">
+                            Shoes
+                        </label>
+                    </div>
+                </div>
+                <div className="filterItem">
+                    <h2>Filter by price</h2>
+                </div>
+                <div className="filterItem">
+                    <h2>Sort by</h2>
+                </div>
+            </div>
+            <div className="right"></div>
+        </div>
     )
 }
 
