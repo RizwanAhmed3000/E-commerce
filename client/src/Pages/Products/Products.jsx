b8cf85819472980b1b31e8f1e90a7beca1cc32aa
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-01-26 12:32:15 +0500

    left part for products page

--- client/src/Pages/Products/Products.jsx
+++ client/src/Pages/Products/Products.jsx
@@ -12,12 +12,41 @@ const Products = () => {
                             Shoes
                         </label>
                     </div>
+                    <div className="inputItem">
+                        <input type="checkbox" id="2" value={2}/>
+                        <label htmlFor="2">
+                            Bags
+                        </label>
+                    </div>
+                    <div className="inputItem">
+                        <input type="checkbox" id="3" value={3}/>
+                        <label htmlFor="3">
+                            Coats
+                        </label>
+                    </div>
                 </div>
                 <div className="filterItem">
                     <h2>Filter by price</h2>
+                    <div className="inputItem">
+                        <span>0</span>
+                        <input type="range" min={0} max={1000}/>
+                        <span>1000</span>
+                    </div>
                 </div>
                 <div className="filterItem">
                     <h2>Sort by</h2>
+                    <div className="inputItem">
+                        <input type="radio" id='asc' value='asc' name='price' />
+                        <label htmlFor="asc">
+                            Price (Low to High)
+                        </label>
+                    </div>
+                    <div className="inputItem">
+                        <input type="radio" id='dsc' value='dsc' name='price' />
+                        <label htmlFor="dsc">
+                            Price (High to Low)
+                        </label>
+                    </div>
                 </div>
             </div>
             <div className="right"></div>
