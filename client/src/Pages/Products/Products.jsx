9fc2d65f89df4f41c9fe74208f5bb9c9cc81528e
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-01-29 12:56:17 +0500

    navbar Sticky, products page Scss

--- client/src/Pages/Products/Products.jsx
+++ client/src/Pages/Products/Products.jsx
@@ -1,7 +1,13 @@
+import { useParams } from 'react-router-dom'
 import List from '../../Components/List/List'
 import './products.scss'
+import { useState } from 'react'
 
 const Products = () => {
+    const categoryId = +(useParams().id)
+    const [maxPrice, setMaxPrice] = useState(1000)
+    const [sort, setSort] = useState(null)
+
     return (
         <div className='products'>
             <div className="left">
@@ -30,20 +36,20 @@ const Products = () => {
                     <h2>Filter by price</h2>
                     <div className="inputItem">
                         <span>0</span>
-                        <input type="range" min={0} max={1000} />
-                        <span>1000</span>
+                        <input type="range" min={0} max={1000} onChange={(e) => setMaxPrice(e.target.value)} />
+                        <span>{maxPrice}</span>
                     </div>
                 </div>
                 <div className="filterItem">
                     <h2>Sort by</h2>
                     <div className="inputItem">
-                        <input type="radio" id='asc' value='asc' name='price' />
+                        <input type="radio" id='asc' value='asc' name='price' onChange={e => setSort("asc")} />
                         <label htmlFor="asc">
                             Price (Low to High)
                         </label>
                     </div>
                     <div className="inputItem">
-                        <input type="radio" id='dsc' value='dsc' name='price' />
+                        <input type="radio" id='dsc' value='dsc' name='price' onChange={e => setSort("dsc")} />
                         <label htmlFor="dsc">
                             Price (High to Low)
                         </label>
@@ -52,7 +58,7 @@ const Products = () => {
             </div>
             <div className="right">
                 <img src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600" alt="" className='catImg' />
-                <List />
+                <List categoryId={categoryId} maxPrice={maxPrice} sort={setSort} />
             </div>
         </div>
     )
