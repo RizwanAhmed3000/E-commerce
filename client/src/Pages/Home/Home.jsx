b9a899f0edc1c82882424fa86f3f28a663fec30d
RizwanAhmed3000 <rizz112khan@gmail.com>
2024-02-20 12:11:25 +0500

    Particles JS

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -3,6 +3,7 @@ import Slider from '../../Components/Slider/Slider'
 import FeaturedProduct from '../../Components/FeaturedProduct/FeaturedProduct'
 import Categories from '../../Components/Categories/Categories'
 import Contact from '../../Components/Contact/Contact'
+// import ParticlesComp from '../../Components/Particles/Particles'
 
 const Home = () => {
     return (
